bc816666bf7b666d124407695739f55de6284845
Ammar Husain <ammo700@gmail.com>
2015-06-15 10:00:40 -0700

    Added logic for button enable and disable.

--- Arduino Code/flow_meter_code/flow_meter_code.ino
+++ Arduino Code/flow_meter_code/flow_meter_code.ino
@@ -19,12 +19,31 @@ unsigned long oldTime;
 //creates json for formatting objects
 Json obj1(String(12345), String("Seconds"), String("Flow_Rate"), String("Temperature"), String("Milliliters_Used"));
 
+//Variables for starting with switch
+
+int inPin = 4;         // the number of the input pin
+int outPin = 13;       // the number of the output pin
+
+boolean state = false;      // the current state of the output pin
+int reading;           // the current reading from the input pin
+boolean previous = false;    // the previous reading from the input pin
+
+// the follow variables are long's because the time, measured in miliseconds,
+// will quickly become a bigger number than can be stored in an int.
+long time = 0;         // the last time the output pin was toggled
+long debounce = 200;   // the debounce time, increase if the output flickers
+
+
 void setup()
 {
   
   // Initialize a serial connection for reporting values to the host
-   Serial.begin(38400);
+   Serial.begin(1200);
+   //Switch
    
+   pinMode(inPin, INPUT);
+  
+  
   // Set up the status LED line as an output
   pinMode(statusLed, OUTPUT);
   digitalWrite(statusLed, HIGH);  // We have an active-low LED attached
@@ -51,70 +70,88 @@ void setup()
  
 void loop()
 {
+   reading = digitalRead(inPin);
    
-   if((millis() - oldTime) > 1000)    // Only process counters once per second
-  { 
-    // Disable the interrupt while calculating flow rate and sending the value to
-    // the host
-    detachInterrupt(sensorInterrupt);
-        
-    // Because this loop may not complete in exactly 1 second intervals we calculate
-    // the number of milliseconds that have passed since the last execution and use
-    // that to scale the output. We also apply the calibrationFactor to scale the output
-    // based on the number of pulses per second per units of measure (litres/minute in
-    // this case) coming from the sensor.
-    flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
-    
-    // Note the time this processing pass was executed. Note that because we've
-    // disabled interrupts the millis() function won't actually be incrementing right
-    // at this point, but it will still return the value it was set to just before
-    // interrupts went away.
+   if (reading == HIGH && previous == false && millis() - time > debounce) {
+    state = true;
     oldTime = millis();
+    while (state == true) {
+    if (reading == HIGH && previous == true && millis() - time > debounce) {
+      state = false;
+    }
+    if((millis() - oldTime) > 1000)    // Only process counters once per second
+      { 
+        // Disable the interrupt while calculating flow rate and sending the value to
+        // the host
+        detachInterrupt(sensorInterrupt);
+            
+        // Because this loop may not complete in exactly 1 second intervals we calculate
+        // the number of milliseconds that have passed since the last execution and use
+        // that to scale the output. We also apply the calibrationFactor to scale the output
+        // based on the number of pulses per second per units of measure (litres/minute in
+        // this case) coming from the sensor.
+        flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
+        
+        // Note the time this processing pass was executed. Note that because we've
+        // disabled interrupts the millis() function won't actually be incrementing right
+        // at this point, but it will still return the value it was set to just before
+        // interrupts went away.
+        oldTime = millis();
+        
+        // Divide the flow rate in litres/minute by 60 to determine how many litres have
+        // passed through the sensor in this 1 second interval, then multiply by 1000 to
+        // convert to millilitres.
+        flowMilliLitres = (flowRate / 60) * 1000;
+        
+        // Add the millilitres passed in this second to the cumulative total
+        totalMilliLitres += flowMilliLitres;
+          
+        unsigned int frac;
+        // Determine the fractional part. The 10 multiplier gives us 1 decimal place.
+        frac = (flowRate - int(flowRate)) * 10;
+        
+        obj1.jsonAdd(String(millis()),String(flowMilliLitres),String(0),String(totalMilliLitres));
+        
+        
+        /*
+        //Print Milliseconds
+        Serial.print("Time: ");
+        Serial.print(millis());
+        Serial.print(".");
+        // Print the flow rate for this second in litres / minute
+        Serial.print("Flow rate: ");
+        Serial.print(int(flowRate));  // Print the integer part of the variable
+        Serial.print(".");             // Print the decimal point
+        
+        Serial.print(frac, DEC) ;      // Print the fractional part of the variable
+        Serial.print("L/min");
+        // Print the number of litres flowed in this second
+        Serial.print("  Current Liquid Flowing: ");             // Output separator
+        Serial.print(flowMilliLitres);
+        Serial.print("mL/Sec");
     
-    // Divide the flow rate in litres/minute by 60 to determine how many litres have
-    // passed through the sensor in this 1 second interval, then multiply by 1000 to
-    // convert to millilitres.
-    flowMilliLitres = (flowRate / 60) * 1000;
-    
-    // Add the millilitres passed in this second to the cumulative total
-    totalMilliLitres += flowMilliLitres;
-      
-    unsigned int frac;
-    // Determine the fractional part. The 10 multiplier gives us 1 decimal place.
-    frac = (flowRate - int(flowRate)) * 10;
-    
-    obj1.jsonAdd(String(millis()),String(flowMilliLitres),String(0),String(totalMilliLitres));
-    
-    Serial.println(obj1.getJson());
-    /*
-    //Print Milliseconds
-    Serial.print("Time: ");
-    Serial.print(millis());
-    Serial.print(".");
-    // Print the flow rate for this second in litres / minute
-    Serial.print("Flow rate: ");
-    Serial.print(int(flowRate));  // Print the integer part of the variable
-    Serial.print(".");             // Print the decimal point
-    
-    Serial.print(frac, DEC) ;      // Print the fractional part of the variable
-    Serial.print("L/min");
-    // Print the number of litres flowed in this second
-    Serial.print("  Current Liquid Flowing: ");             // Output separator
-    Serial.print(flowMilliLitres);
-    Serial.print("mL/Sec");
-
-    // Print the cumulative total of litres flowed since starting
-    Serial.print("  Output Liquid Quantity: ");             // Output separator
-    Serial.print(totalMilliLitres);
-    Serial.println("mL"); 
-    */
-    Serial.println(obj1.getJson());
-    // Reset the pulse counter so we can start incrementing again
-    pulseCount = 0;
-    
-    // Enable the interrupt again now that we've finished sending output
-    attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+        // Print the cumulative total of litres flowed since starting
+        Serial.print("  Output Liquid Quantity: ");             // Output separator
+        Serial.print(totalMilliLitres);
+        Serial.println("mL"); 
+        */
+        
+        // Reset the pulse counter so we can start incrementing again
+        pulseCount = 0;
+        
+        // Enable the interrupt again now that we've finished sending output
+        attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+      }
+    }
+  Serial.println(obj1.getJson());
+    time = millis();    
   }
+
+  
+
+  previous = false;
+
+   
 }
 
 
