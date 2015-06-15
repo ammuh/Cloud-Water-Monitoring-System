df3782ed4c6a563352b994e1d3bede89ad261925
Ammar Husain <ammo700@gmail.com>
2015-06-15 10:57:24 -0700

    Added button functionality to start and stop measurement process

--- Arduino Code/flow_meter_code/flow_meter_code.ino
+++ Arduino Code/flow_meter_code/flow_meter_code.ino
@@ -38,7 +38,7 @@ void setup()
 {
   
   // Initialize a serial connection for reporting values to the host
-   Serial.begin(1200);
+   Serial.begin(38400);
    //Switch
    
    pinMode(inPin, INPUT);
@@ -73,85 +73,71 @@ void loop()
    reading = digitalRead(inPin);
    
    if (reading == HIGH && previous == false && millis() - time > debounce) {
-    state = true;
-    oldTime = millis();
-    while (state == true) {
-    if (reading == HIGH && previous == true && millis() - time > debounce) {
-      state = false;
-    }
-    if((millis() - oldTime) > 1000)    // Only process counters once per second
-      { 
-        // Disable the interrupt while calculating flow rate and sending the value to
-        // the host
-        detachInterrupt(sensorInterrupt);
-            
-        // Because this loop may not complete in exactly 1 second intervals we calculate
-        // the number of milliseconds that have passed since the last execution and use
-        // that to scale the output. We also apply the calibrationFactor to scale the output
-        // based on the number of pulses per second per units of measure (litres/minute in
-        // this case) coming from the sensor.
-        flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
-        
-        // Note the time this processing pass was executed. Note that because we've
-        // disabled interrupts the millis() function won't actually be incrementing right
-        // at this point, but it will still return the value it was set to just before
-        // interrupts went away.
-        oldTime = millis();
-        
-        // Divide the flow rate in litres/minute by 60 to determine how many litres have
-        // passed through the sensor in this 1 second interval, then multiply by 1000 to
-        // convert to millilitres.
-        flowMilliLitres = (flowRate / 60) * 1000;
-        
-        // Add the millilitres passed in this second to the cumulative total
-        totalMilliLitres += flowMilliLitres;
-          
-        unsigned int frac;
-        // Determine the fractional part. The 10 multiplier gives us 1 decimal place.
-        frac = (flowRate - int(flowRate)) * 10;
-        
-        obj1.jsonAdd(String(millis()),String(flowMilliLitres),String(0),String(totalMilliLitres));
-        
-        
-        /*
-        //Print Milliseconds
-        Serial.print("Time: ");
-        Serial.print(millis());
-        Serial.print(".");
-        // Print the flow rate for this second in litres / minute
-        Serial.print("Flow rate: ");
-        Serial.print(int(flowRate));  // Print the integer part of the variable
-        Serial.print(".");             // Print the decimal point
-        
-        Serial.print(frac, DEC) ;      // Print the fractional part of the variable
-        Serial.print("L/min");
-        // Print the number of litres flowed in this second
-        Serial.print("  Current Liquid Flowing: ");             // Output separator
-        Serial.print(flowMilliLitres);
-        Serial.print("mL/Sec");
+    state = true; //Change state
+    oldTime = millis(); //reset oldTime to compensate for time user takes to press button
+    time = millis(); //switch was toggled so need to reset time for next logic gate
+    unsigned long timeStart = millis();
+    Serial.println("Switch Activated, measure loop began");
     
-        // Print the cumulative total of litres flowed since starting
-        Serial.print("  Output Liquid Quantity: ");             // Output separator
-        Serial.print(totalMilliLitres);
-        Serial.println("mL"); 
-        */
+    while (state == true) {
         
-        // Reset the pulse counter so we can start incrementing again
-        pulseCount = 0;
+      
+      if (digitalRead(inPin) == HIGH && millis() - time > debounce) {
+        Serial.println("Swith Toggled");
+        state = false;
         
-        // Enable the interrupt again now that we've finished sending output
-        attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
       }
+      else {
+        //Continues with measurement process
+        if((millis() - oldTime) > 1000)    // Only process counters once per second
+          { 
+            // Disable the interrupt while calculating flow rate and sending the value to
+            // the host
+            detachInterrupt(sensorInterrupt);
+            Serial.println("In measurement");    
+            // Because this loop may not complete in exactly 1 second intervals we calculate
+            // the number of milliseconds that have passed since the last execution and use
+            // that to scale the output. We also apply the calibrationFactor to scale the output
+            // based on the number of pulses per second per units of measure (litres/minute in
+            // this case) coming from the sensor.
+            flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
+            
+            // Note the time this processing pass was executed. Note that because we've
+            // disabled interrupts the millis() function won't actually be incrementing right
+            // at this point, but it will still return the value it was set to just before
+            // interrupts went away.
+            oldTime = millis();
+            
+            // Divide the flow rate in litres/minute by 60 to determine how many litres have
+            // passed through the sensor in this 1 second interval, then multiply by 1000 to
+            // convert to millilitres.
+            flowMilliLitres = (flowRate / 60) * 1000;
+            
+            // Add the millilitres passed in this second to the cumulative total
+            totalMilliLitres += flowMilliLitres;
+              
+            unsigned int frac;
+            // Determine the fractional part. The 10 multiplier gives us 1 decimal place.
+            frac = (flowRate - int(flowRate)) * 10;
+            
+            obj1.jsonAdd(String(millis()-timeStart),String(flowMilliLitres),String(0),String(totalMilliLitres));
+                  
+            // Reset the pulse counter so we can start incrementing again
+            pulseCount = 0;
+            
+            // Enable the interrupt again now that we've finished sending output
+            attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+          }
+        
+      }
+      
     }
-  Serial.println(obj1.getJson());
+    Serial.println("Exited while Loop");
+    Serial.println(obj1.getJson());
     time = millis();    
   }
-
-  
-
-  previous = false;
-
-   
+  //reset previous setting
+  previous = false;   
 }
 
 
