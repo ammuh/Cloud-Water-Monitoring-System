dd571ba10eaf11dabcaa0fb6537ce07d9928a45a
Ammar Husain <ammo700@gmail.com>
2015-06-22 17:02:41 -0700

    Added final base file for Fluxduino code, inputted flow meter and pushbutton template

---
+++ Arduino Code/Fluxduino/Fluxduino.ino
@@ -0,0 +1,57 @@
+/*
+Code for Fluxduino
+*/
+
+//Button variables
+int buttonPin = 3;   // choose the input pin (for a pushbutton)
+boolean state = false;     // variable for reading the pin status
+
+//Flow Meter Variabls
+byte sensorInterrupt = 0;  // 0 = digital pin 2
+byte sensorPin       = 2;
+// The hall-effect flow sensor outputs approximately 4.5 pulses per second per
+// litre/minute of flow.
+float calibrationFactor = 4.5;
+volatile byte pulseCount;  
+float flowRate;
+unsigned int flowMilliLitres;
+unsigned long totalMilliLitres;
+unsigned long oldTime;
+
+
+void setup() {
+  //Pushbutton Setup
+  pinMode(buttonPin, INPUT);    // declare pushbutton as input
+  
+  //Flow Meter Setup
+  pinMode(sensorPin, INPUT);
+  digitalWrite(sensorPin, HIGH);
+
+  pulseCount        = 0;
+  flowRate          = 0.0;
+  flowMilliLitres   = 0;
+  totalMilliLitres  = 0;
+  oldTime           = 0;
+  
+
+  // The Hall-effect sensor is connected to pin 2 which uses interrupt 0.
+  // Configured to trigger on a FALLING state change (transition from HIGH
+  // state to LOW state)
+  attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+}
+
+void loop(){  
+  //Halts till Push Button is pressed
+  while (digitalRead(buttonPin) == LOW){
+  }
+  
+}
+
+/*Extra Methods*/
+
+//For Flow Meter
+void pulseCounter()
+{
+  // Increment the pulse counter
+  pulseCount++;
+}
