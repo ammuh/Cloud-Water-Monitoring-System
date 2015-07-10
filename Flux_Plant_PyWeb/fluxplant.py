84e242541953c3fbcb38d01ae09524d90bf5e3e4
Ammar Husain <ammo700@gmail.com>
2015-07-10 08:24:47 -0700

    Added Web App for URL handling and serving web pages

--- Flux_Plant_PyWeb/fluxplant.py
+++ Flux_Plant_PyWeb/fluxplant.py
@@ -1,16 +1,10 @@
 import webapp2
-from google.appengine.api import users
 
-class MyHandler(webapp2.RequestHandler):
+class MainPage(webapp2.RequestHandler):
     def get(self):
-        user = users.get_current_user()
-        if user:
-            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
-                        (user.nickname(), users.create_logout_url('/')))
-        else:
-            greeting = ('<a href="%s">Sign in or register</a>.' %
-                        users.create_login_url('/'))
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write('Hello, webapp2 World!')
 
-        self.response.out.write("<html><body>%s</body></html>" % greeting)
-
-application = webapp2.WSGIApplication([('/', MyHandler),], debug=True)
+application = webapp2.WSGIApplication([
+    ('/', MainPage)
+], debug=True)
